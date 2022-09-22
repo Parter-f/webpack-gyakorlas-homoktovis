dc28825634d1589a86a7b5152d6f7ced07f3d5fd
Árpás Péter <arpaspeter0@gmail.com>
2022-09-22 15:55:05 +0200

    kellett a help azért

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.css';
-import'bootstrap/dist/js/bootstrap.bundle.js';
+import 'bootstrap/dist/js/bootstrap.bundle.js';
 import './style.css';
 
 console.log('Loaded');
